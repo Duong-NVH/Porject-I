abc66e3c80beadfb5c73fdf2159811c1dc45a591
Duong Dep Trai <devodo123i@gmail.com>
2020-04-25 17:51:12 +0700

    Updates

---
+++ precache-manifest.e136a367669dcd9e47fabd98e82be7f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ea6573120dba2b19591a2458178910",
+    "url": "/Project-I/index.html"
+  },
+  {
+    "revision": "34fa5a5ba14ea2c43b00",
+    "url": "/Project-I/static/css/main.8e30f9a4.chunk.css"
+  },
+  {
+    "revision": "41466f60bd403808aa11",
+    "url": "/Project-I/static/js/2.74953b70.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/Project-I/static/js/2.74953b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fa5a5ba14ea2c43b00",
+    "url": "/Project-I/static/js/main.d0a07c86.chunk.js"
+  },
+  {
+    "revision": "967633dfd8990ab9a63e",
+    "url": "/Project-I/static/js/runtime-main.1698e1e0.js"
+  }
+]);
